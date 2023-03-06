f5f1afd36046c665e1841b481bd9067b9edc923e
Michael David Vinc <tawie18102000@gmail.com>
2023-03-06 22:48:10 +0700

    SELESAI

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import UserRoutes from "./routes/UserRoute.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoutes);
+
+
+app.listen('5000', () => console.log('Server up and Running...'));
